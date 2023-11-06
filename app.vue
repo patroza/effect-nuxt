7508957f36c0a941eccfb254bd37f473804babf5
Patrick Roza <contact@patrickroza.com>
2023-11-06 08:33:34 +0100

    initial

---
+++ app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    Hello {{ effect }}
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Effect } from "effect"
+
+const effect = Effect.succeed("world").pipe(Effect.runSync)
+
+</script>
