7895b5e719428cfef6206be6d4ab92f71ec0f509
Patrick Roza <contact@patrickroza.com>
2023-11-08 00:11:40 +0100

    bump

--- app.vue
+++ app.vue
@@ -1,12 +1,30 @@
 <template>
   <div>
-    Hello {{ effect }}
+    Hello {{ effect }} {{ effect2 }}
   </div>
 </template>
 
 <script setup lang="ts">
+import { Option, Schedule } from "effect"
 import { Effect } from "effect"
+import * as Effect2 from "effect/Effect"
+
+type TestType = Effect<never, never, number>
+
+
+const a: Effect<string, string, void> = Effect.succeed("hello")
 
 const effect = Effect.succeed("world").pipe(Effect.runSync)
+const effect2 = Effect2.succeed("x2").pipe(Effect.runSync)
+
+// const shouldFailCompiler: Effect<never, never, string> = Effect.succeed(1)
+
+const ao = Option.none()
+const bo = Option.some(1)
+const co = bo.pipe((_) => Option.map((_) => 2)(_))
+
+const as = Schedule.identity()
+const bs = as.pipe((_) => Schedule.map((_) => 2)(_))
+
 
 </script>
